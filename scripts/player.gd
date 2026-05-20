5a10dc4ca9e8f96c2b10b607441259411d8d2eac
Ben <ben.tom.vincent@gmail.com>
2026-05-20 15:54:39 -0700

    updated physics to behave with move_and_collide instead of move_and_slide and now bounces and has some fall off

--- scripts/player.gd
+++ scripts/player.gd
@@ -16,17 +16,26 @@ func _physics_process(delta: float) -> void:
 
 	if not is_on_floor() and launched:
 		velocity += get_gravity() * delta
-
+	
+	
+	# NOTICE JUMPING
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+	
+	# NOTICE MOVING 
 	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
+	
+	if direction and not launched:
 		velocity.x = direction * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, velocity.x * 0.9, SPEED)
+	
+	var collision = move_and_collide(velocity * delta)
 
-	move_and_slide()
+	# NOTICE BOUNCING
+	if collision:
+		velocity = velocity.bounce(collision.get_normal())
+		velocity = Vector2(velocity.x * 0.75, velocity.y * 0.75)
 
 
 
