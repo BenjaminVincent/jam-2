f76855ecac74b6ffb3c5f35da11aa8100799cd41
Ben <ben.tom.vincent@gmail.com>
2026-05-20 14:01:08 -0700

    updated to fullscreen

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,12 +1,8 @@
 extends Control
 
 
-func _ready() -> void:
-	print("READY!")
-
-func _on_gui_input(event: InputEvent) -> void:
-	print("test: ", event)
-	if event.is_action_just_pressed("FULLSCREEN"):
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("FULLSCREEN"):
 		if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN:
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 		else:
