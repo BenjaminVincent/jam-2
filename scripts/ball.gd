2cca39b333402cf00e4789f2f33b4da269b1d721
Ben <ben.tom.vincent@gmail.com>
2026-05-22 14:56:31 -0700

    I'M DONE

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -3,6 +3,8 @@ extends RigidBody2D
 var initial_position: Vector2
 var current_bin: StaticBody2D
 
+@onready var game = get_node("/root/Game")
+
 func _ready() -> void:
 	freeze = true
 	initial_position = position
@@ -11,33 +13,39 @@ func _ready() -> void:
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("RESET_BALL"):
-		#_reset_ball(initial_position)
-		GameState._reset_game()
+		_reset_ball()
 		
 	if Input.is_action_just_pressed("LAUNCH"):
 		freeze = false
 	
 
 
-func _reset_ball(spawn_pos: Vector2 = initial_position) -> void:
+func _reset_ball() -> void:
+	var _nothing = global_position # CRITICAL NEVEVER REMOVE THIS OR ENTIRE GAME BREAKS
 	freeze = false
-	var _transform := Transform2D(0.0, spawn_pos)
-	PhysicsServer2D.body_set_state(get_rid(), PhysicsServer2D.BODY_STATE_TRANSFORM, _transform)
-	PhysicsServer2D.body_set_state(get_rid(), PhysicsServer2D.BODY_STATE_LINEAR_VELOCITY, Vector2.ZERO)
-	PhysicsServer2D.body_set_state(get_rid(), PhysicsServer2D.BODY_STATE_ANGULAR_VELOCITY, 0.0)
+	global_position = initial_position
+	linear_velocity = Vector2.ZERO
+	angular_velocity = 0.0
+	rotation = 0.0
+	
+	await get_tree().physics_frame
 	freeze = true
+	GameState.score = 0
+	game._on_update_score()
+
 
 
 func _on_body_entered(body: Node) -> void:
-	#print("sleeping: ", sleeping)
-	#print("body: ", body)
 	if body.has_meta("type") and body.get_meta("type") == "collider":
 		body._on_hit()
 	if body.has_meta("type") and body.get_meta("type") == "bin":
 		current_bin = body
 
 
+
 func _on_sleeping_state_changed() -> void:
-	if current_bin:
+	if sleeping and current_bin:
 		current_bin._on_hit()
+		current_bin = null
+		
 	
