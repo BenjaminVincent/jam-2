16f6689fea92bbbf1bb3d899581ecef5b06eef9e
Jazeck1 <jzsaxophone@gmail.com>
2026-05-21 18:54:47 -0700

    add tool setter to bin to update point value in editor

--- scripts/bin.gd
+++ scripts/bin.gd
@@ -1,13 +1,25 @@
+@tool
 extends StaticBody2D
 
+@export var point_value: int = 5:
+	set(value):
+		point_value = value
+		_update_label()
+
 @onready var score_label: RichTextLabel = $ScoreLabel
-@export var point_value: int = 5
 
 
 
 func _ready() -> void:
+	_update_label()
 	set_meta("type", "bin")
 	score_label.text = str(point_value)
+
+
+
+func _update_label() -> void:
+	if is_node_ready() and score_label:
+		score_label.text = str(point_value)
 
 
 
