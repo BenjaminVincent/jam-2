3d2da441dba825edf85fcb9098faada491f6cb37
Ben <ben.tom.vincent@gmail.com>
2026-05-24 15:18:55 -0700

    added display numbers

--- scripts/basic_collider.gd
+++ scripts/basic_collider.gd
@@ -20,7 +20,25 @@ func _on_hit() -> void:
 	
 	GameState.add_to_score(point_value)
 	
+	if point_value > 0:
+		var display_number = load("res://scenes/display_number.tscn").instantiate()
+		add_child(display_number)
+		tween_number(display_number)
+		print("adding child!")
+	
+	
 	sprite_2d.texture = hit_texture
 	await get_tree().create_timer(0.3).timeout
 	await get_tree().process_frame
 	sprite_2d.texture = base_texture
+
+
+func tween_number(node) -> void:
+	var tween = create_tween()
+	tween.tween_property(node, "scale", Vector2(2, 2), 0.6).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_ELASTIC)
+	tween.tween_property(node, "position", Vector2(0, -64), 1.1).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
+	tween.tween_interval(0.6)
+	tween.tween_property(node, "scale", Vector2(1, 1), 0.3).set_trans(Tween.TRANS_SINE)
+	tween.tween_property(node, "modulate:a", 0.0, 0.5)
+	await tween.finished
+	node.queue_free()
